82edee505a0cd6a0d2e3d4a12692134a773ca588
Thalis Ribeiro <thalis199@outlook.com>
2022-06-07 18:32:12 -0300

    Novo if para nao travar o app caso a pessoa aperte backspace antes de outro botao

--- Calculadora/XamarinCalc/XamarinCalc/ViewModels/MainPageViewModel.cs
+++ Calculadora/XamarinCalc/XamarinCalc/ViewModels/MainPageViewModel.cs
@@ -16,25 +16,31 @@ namespace XamarinCalc.ViewModels
 
         public string Entrada { get => entrada; set => SetProperty(ref entrada, value); }
         string entrada;
-
-
+        ModeloCalculadora calculadora;
+        bool canUseSpecialButtons;
         public MainPageViewModel()
         {
             AdicionaNumeroComando = new Command<string>(AdicionaNumero);
             ExcluiUltimoComando = new Command(ExcluiUltimo);
             ExcluiTodasComando = new Command(ExcluiTodas);
+            canUseSpecialButtons = false;
         }
 
         void AdicionaNumero(string botao)
         {
+            canUseSpecialButtons = true;
             Entrada+=botao;
         }
         void ExcluiUltimo()
         {
+            if (canUseSpecialButtons == false || Entrada.Length == 0 )
+                return;
             Entrada = Entrada.Remove(Entrada.Length-1);
         }
         void ExcluiTodas()
         {
+            if (canUseSpecialButtons == false || Entrada.Length == 0)
+                return;
             Entrada = Entrada.Remove(0);
         }
     }
